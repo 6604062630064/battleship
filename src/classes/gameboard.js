6cdc972bb1d03eb2c905f15d58d54443803ccb48
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2023-04-28 11:54:09 +0700

    Add an Add method in the Gameboard class

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -1,13 +1,74 @@
 class Gameboard {
-	constructor() {
+	constructor(size) {
 		// Make 10x10 2d array
-		this.playingField = [...Array(10)].map((e) =>
-			Array(10).fill({ ship: null, isHit: false })
-		);
+		this.size = size;
+		this.playingField = (function () {
+			let arr = [];
+			for (let i = 0; i < size; i++) {
+				arr.push([]);
+
+				for (let j = 0; j < size; j++) {
+					arr[i].push({ ship: null, isHit: false });
+				}
+			}
+
+			return arr;
+		})(); // IIFE function
 	}
 
 	getTileAt(i, j) {
-		return this.playingField[i][j];
+		if (i < this.size && j < this.size) {
+			return this.playingField[i][j];
+		} else {
+			throw new Error("Out of bound!");
+		}
+	}
+
+	add(obj) {
+		const column = obj.cord[0];
+		const row = obj.cord[1];
+		const ship = obj.ship;
+		const pos = obj.position;
+
+		const checkIfEmpty = (position) => {
+			// a function for checking if another ship exists or shis are out of bound
+			const getTile = (i) => {
+				return position === "vertical"
+					? this.getTileAt(column, row + i)
+					: this.getTileAt(column + i, row);
+			};
+
+			for (let i = 0; i <= ship.length; i++) {
+				const currentPos = getTile(i);
+
+				if (currentPos.ship !== null) {
+					throw new Error("Cannot place in an occupied position!");
+				}
+			}
+		};
+
+		if (pos === "vertical") {
+			checkIfEmpty("vertical");
+
+			for (let i = 0; i <= ship.length; i++) {
+				// a fucntion for placing ships
+				const currentPos = this.getTileAt(column, row + i);
+
+				currentPos.ship = ship;
+			}
+			return;
+		}
+
+		if (pos === "horizontal") {
+			checkIfEmpty("horizontal");
+
+			for (let j = 0; j <= ship.length; j++) {
+				const currentPos = this.getTileAt(column + j, row);
+
+				currentPos.ship = ship;
+			}
+			return;
+		}
 	}
 }
 
