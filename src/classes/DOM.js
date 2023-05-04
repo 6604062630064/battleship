52d2a85888e0e304119bf4d49b2701b957b45b0d
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2023-05-05 00:40:27 +0700

    Add prepareShips function

--- src/classes/DOM.js
+++ src/classes/DOM.js
@@ -1,6 +1,7 @@
+const tables = document.querySelectorAll(".bigger-table > div > div");
+
 const initialize = () => {
 	// Add starting cells in both tables
-	const tables = document.querySelectorAll(".bigger-table > div > div");
 	tables.forEach((e) => {
 		for (let i = 0; i < 10; i++) {
 			for (let j = 0; j < 10; j++) {
@@ -13,6 +14,30 @@ const initialize = () => {
 			}
 		}
 	});
+
+	const button = document.querySelector("button");
+
+	// Add an event to start the game
+	button.addEventListener("click", (e) => {
+		e.preventDefault();
+		startGame();
+
+		e.target.disabled = true;
+	});
 };
 
-export default initialize;
+const prepareShips = (arr) => {
+	// Get info from the array then display it to
+	arr.forEach((e, i) => {
+		e.forEach((child, j) => {
+			if (child.ship !== null) {
+				const cell = document.querySelector(
+					`.player [row="${i}"][column="${j}"]`
+				);
+
+				cell.classList.add("occupied");
+			}
+		});
+	});
+};
+export { initialize, prepareShips };
